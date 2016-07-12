9be8aab4d9fca99219fd7590b89dc4c9c88aa8aa
Johnnie de La Moriniere <delamoriniere@Johnnies-MBP.home>
2016-07-12 14:17:56 +0100

    update index, style and main js files for end of unit 11, added root directory with card images

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,57 @@
 console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
-var cardFour = "king";
+var cardFour = "king";*/
 
-if (cardTwo === cardFour) {
-	alert("You found a match!");
-}
-else {
-	alert("Sorry, try again.");
-}
+var gameBoard = document.getElementById('game-board');
+
+var cards = ['queen', 'king', 'king', 'queen'];
+
+var cardsInPlay = [];
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i++) {
+    var newCard = document.createElement('div');
+    newCard.className = "card";
+    newCard.setAttribute('data-card', cards[i]);
+    newCard.addEventListener('click', isTwoCards);
+    gameBoard.appendChild(newCard);
+  }
+};
+
+
+var isMatch = function(array){
+	(array[0] === array[1]) ?   alert("You found a match!") :  alert("Sorry, try again.");
+      // clears innerHTML from all cards (turns them back over)
+    for (var i = 0; i < cards.length; i++) {
+    document.getElementsByClassName('card')[i].innerHTML = " ";
+  }
+};
+
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this prework, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  this.innerHTML = '<img src="root/' + this.getAttribute('data-card') + '.png">';
+
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for next try
+    cardsInPlay = [];
+
+  }
+
+
+};
+
+createBoard(cards.length);
+
